5d1755ba30c6479dddd69de38f13193588ed7978
adem kok <ademk7604@gmail.com>
2023-10-27 11:28:05 +0200

    first commit

---
+++ src/multiCleintChatApp/ClientHandler.java
@@ -0,0 +1,65 @@
+package multiCleintChatApp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+	private Socket client;
+	private BufferedReader input;
+	private PrintWriter output;
+	private static ArrayList<ClientHandler> clients;
+	
+	public ClientHandler(Socket clientSocket, ArrayList<ClientHandler> clients) throws IOException {
+		client = clientSocket;
+		this.clients = clients;
+		input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		output = new PrintWriter(client.getOutputStream(), true);
+	}
+
+	@Override
+	public void run() {
+		try {
+			while(true) {
+				String request =input.readLine();
+				if(request.contains("bir kisi soyle")) {
+					output.println(Server.rastgeleKisiSec());
+				}else if(request.startsWith("[herkes]")) {
+					int ilkBosluk = request.indexOf(" ");
+					if(ilkBosluk != -1) {
+						//butun kullanicilara mesaji yolla
+						butunKisilereMesajGonder(request.substring(ilkBosluk+1));
+						
+					}
+				} else if(request.equals("Exit")) {
+					output.println("Hoscakal yine bekleriz");
+					break;
+				}else {
+					output.println("Rastgele bir kisi icin \"bir kisi soyle\" yaziniz!");
+				}
+			}
+		}catch(IOException e) {
+			System.out.println("Client Handler da hata olustu");
+			e.printStackTrace();
+		} finally {
+			output.close();
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	private void butunKisilereMesajGonder(String msg) {
+		for(ClientHandler tempClient: clients) {
+			tempClient.output.println(Thread.currentThread().getName()+": "+msg);
+		}
+	}
+	
+
+}
---
+++ src/multiCleintChatApp/Server.java
@@ -0,0 +1,56 @@
+package multiCleintChatApp;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+	private static final String[] isimler = {"Lea","Jonas","Johannes","Migen"};
+	private static final String[] meslekler = {"Entwickler","Arzt","Lehrer","Polizist"};
+	
+	private static final String SERVER_IP = "127.0.0.1";
+	private static final int PORT = 5003;
+	private static ExecutorService pool = Executors.newFixedThreadPool(3);
+	private static ArrayList<ClientHandler> clients = new ArrayList<>();
+	
+	public static ArrayList<ClientHandler> getClients(){
+		return clients;
+	}
+	
+	//Thread lari yonetmek icin bir havuz olusturalim. en fazla 3 cleint a hizmetvermek istiyoruz
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		ServerSocket serverSocket = new ServerSocket(PORT);
+		
+		while(true) {
+			System.out.println("[SERVER] The server is warting to connection a client...");
+			Socket client = serverSocket.accept();
+			System.out.println("[SERVER] a client connected!");
+			//istemci threadi
+			ClientHandler clientThread = new ClientHandler(client, clients);
+			
+			//clients icersine client thredi ekler
+			clients.add(clientThread);
+			
+			//istemciyi pool da aktif edecegiz
+			pool.execute(clientThread);
+			
+		}
+		
+		
+		
+		
+		
+	}
+	public static String rastgeleKisiSec() {
+		int no = (int)(Math.random()*isimler.length);
+		return isimler[no]+" "+meslekler[no];
+	}
+
+}
